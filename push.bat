1b368df143787a95b0951a89fc96c871b24f5b84
Kataktok <98163876+Kataktok@users.noreply.github.com>
2026-04-03 15:54:34 +0400

    commit

---
+++ push.bat
@@ -0,0 +1,23 @@
+@echo off
+chcp 65001 >nul
+echo ==================================
+echo Добавляем файлы...
+git add .
+echo Готово!
+
+echo.
+echo Делаем коммит...
+git commit -m "commit"
+echo Готово!
+
+echo.
+echo Отправляем на GitHub...
+git push --force
+echo Готово!
+
+echo.
+echo ==================================
+echo ВСЁ ЗАПИСАЛОСЬ!
+echo ==================================
+echo.
+pause
